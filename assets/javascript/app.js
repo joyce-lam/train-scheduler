f2cbfde9d7f7fc774268161aee8a727d7e395022
Joyce <joycelamsaungan@gmail.com>
2018-02-02 21:36:56 -0800

    modifying app.js with added function to display next train arrival

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -51,17 +51,56 @@ function renderRows(snap) {
     var trainRowName = $("<td>").text(snap.trainName);
     var trainRowDestination = $("<td>").text(snap.trainDestination);
     var trainRowFreq = $("<td>").text(snap.trainFreq);
-    var trainRowArrival = $("<td>").text(calculateArrival(snap.trainFreq));
+    var trainRowArrival = $("<td>").text(calculateArrival(snap.trainTime, snap.trainFreq));
     console.log(trainRowArrival);
     // var trainRowMinute = $("<td>").text();
     trainRow.append(trainRowName, trainRowDestination, trainRowFreq, trainRowArrival);
     $(".table").append(trainRow);
 }
 
-function calculateArrival(minute) {
-    var arrivalTime = moment().add(minute, "m");
-    console.log(arrivalTime);
-    console.log(arrivalTime._d);
+function calculateArrival(trainTime, freqMinute) {
+    console.log(trainTime);
+    console.log(freqMinute);
 
+    var startHHmm = trainTime.split(":")
+    startInMin = trainTime[0]*60 + trainTime[1];
+    console.log(startInMin);
+
+    var now = moment().format("HH:mm");
+    console.log(now);
+    var nowHHmm = now.split(":")
+    console.log(nowHHmm);
+    var nowInMin = parseInt(nowHHmm[0]*60) + parseInt(nowHHmm[1]);
+    console.log(nowInMin);
+
+    var diff = nowInMin - startInMin;
+    console.log(diff);
+
+    var noOfTrains = Math.ceil(diff / freqMinute);
+    console.log(noOfTrains);
+
+    var minutesInDay = ((noOfTrains*freqMinute - diff) + nowInMin);
+    console.log(minutesInDay);
+
+    var hours = Math.floor(minutesInDay/60);
+    var minutes = (minutesInDay - hours*60);
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    } 
+
+    if (minutes === 0) {
+        minutes = "00";
+    } else if (minutes <10) {
+        minutes = "0" + minutes;
+    }
+
+    var nextTrainTime = hours + ":" + minutes;
+    console.log(nextTrainTime);
+    return nextTrainTime;
 }
+
+
+
+
 
